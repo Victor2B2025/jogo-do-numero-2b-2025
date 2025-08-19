8f9bea3badac57b803362e9122274dbdc372d062
Victor <victor.martins.santos05@escola.pr.gov.br>
2025-08-19 10:47:46 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-
+let numeroAleatorio = Math.floor(Math.random() *100) + 1;
 const palpites = document.querySelector('.palpites');
 const ultimoResultado = document.querySelector('.ultimoResultado');
 const baixoOuAlto = document.querySelector('.baixoOuAlto');
 const envioPalpite = document.querySelector('.envioPalpite');
 const campoPalpite = document.querySelector('.campoPalpite');
-
-
+let contagemPalpites = 1;
+let botaoReiniciar;
                                             
 function verificarPalpite() {
   const palpiteUsuario = Number(campoPalpite.value);
@@ -17,5 +17,45 @@ palpites.textContent += palpiteUsuario + " ";
 
 if (palpiteUsuario === numeroAleatorio) {
   ultimoResultado.textContent = "Parabéns! Você Acertou!";
-
+  ultimoResultado.style.backgroundColor = "green";
+  baixoOuAlto.textContent = "";
+  finalizarJogo();
+} else if (contagemPalpites === 10) {
+  ultimoResultado.textcontent = "FIM DE JOGO!!";
+  baixoOuAlto.textcontent = "";
+  finalizarJogo();
+} else {
+  ultimoResultado.textContent = "Errado";
+  ultimoResultado.style.backgroundColor = "red";
+  if (palpiteUsuario < numeroAleatorio) {
+    baixoOuAlto.textContent = "O último palpite foi muito baixo";
+  } else if (palpiteUsuario > numeroAleatorio) {
+    baixoOuAlto.textContent = "O último palpite foi muito alto";
+  }
 }
+
+contagemPalpites++;
+campoPalpite.value = "";
+campoPalpite.focus();
+}
+
+envioPalpite.addEventListener('click', verificarPalpite);
+
+function finalizarJogo() {
+  campoPalpite.disabled = true;
+  envioPalpite.disabled = true;
+  botaoReiniciar = document.createElement('button');
+  document.body.appendChild(botaoReiniciar);
+  botaoReiniciar.textContent = 'Reiniciar Jogo';
+  botaoReiniciar.classList.add('botaoReiniciar');
+  document.body.appendChild(botaoReiniciar);
+  botaoReiniciar.addEventListener('click', reiniciarJogo);
+}
+
+function reiniciarJogo() {
+  contagemPalpites = 1;
+  const paragrafosReiniciar = document.querySelectorAll('.paragrafosResultado p');
+  for (const paragrafoReiniciar of paragrafosReiniciar) {
+    paragrafoReiniciar.textcontent = "";
+}
+} 
